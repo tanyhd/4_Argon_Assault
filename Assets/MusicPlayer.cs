59caf884964cea51aa399f4a18c57f001feb9313
dtan_yh85 <darren243@hotmail.com>
2018-11-06 17:36:59 +0800

    Video 80 - Setup A Splash Screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    void Start()
+    {
+        Invoke("LoadStartScene", 5f);
+    }
+
+
+    void LoadStartScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
