f3c133f7a6ed9d5e617c9ba125e331f5bd117cc6
dtan_yh85 <darren243@hotmail.com>
2018-11-08 22:19:52 +0800

    Video 86 - Mathf.Clamp() To Restrict Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,16 +5,21 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip ("In ms^-1")] [SerializeField] float xSpeed = 4f; 
-	// Use this for initialization
-	void Start () {
+    [Tooltip ("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 6f;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        print(xOffsetThisFrame);
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampXPos, transform.localPosition.y, transform.localPosition.z);
+
 	}
 }
