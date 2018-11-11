9d75211c18bab0d95962f039cc251a7e4c986c9a
dtan_yh85 <darren243@hotmail.com>
2018-11-11 23:34:45 +0800

    Video 95 - Project Tidying Checklist

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,28 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour {
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
-    }
-
-    void Start()
-    {
-        Invoke("LoadStartScene", 5f);
-    }
-
-    void LoadStartScene()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    void Start()
+    {
+        Invoke("LoadStartScene", 5f);
+    }
+
+    void LoadStartScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
