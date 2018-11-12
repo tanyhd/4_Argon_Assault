3ae83d248f0786d95b6e5ca789e6134360b65219
dtan_yh85 <darren243@hotmail.com>
2018-11-12 16:21:11 +0800

    Video 100 - Detecting Particle Collisions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject explosionVFX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(explosionVFX, transform.localPosition, Quaternion.identity);
+        print("Hit by bullets");
+        Destroy(gameObject);
+    }
+}
